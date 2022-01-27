49eb4cf1fd6d657f5b776ffc188079640bd589d9
pathiphon <pathiphon.ja@chococrm.com>
2022-01-27 23:40:34 +0700

    Shared preferences on fragment

--- app/src/main/java/com/adedom/dataflow/SharedPreferencesV2Activity.kt
+++ app/src/main/java/com/adedom/dataflow/SharedPreferencesV2Activity.kt
@@ -4,10 +4,16 @@ import android.app.Activity
 import android.content.Context
 import android.content.SharedPreferences
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.edit
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.commit
 import androidx.lifecycle.ViewModel
-import com.adedom.dataflow.databinding.ActivityDataFlowBinding
+import com.adedom.dataflow.databinding.ActivitySharedPreferenceV2Binding
+import com.adedom.dataflow.databinding.FragmentSharedPreferenceV2Binding
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 abstract class BaseActivity : AppCompatActivity(),
@@ -34,10 +40,35 @@ abstract class BaseActivity : AppCompatActivity(),
     }
 }
 
+abstract class BaseFragment : Fragment(),
+    SharedPreferences.OnSharedPreferenceChangeListener {
+
+    private var sharedPreferences: SharedPreferences? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        sharedPreferences =
+            context?.getSharedPreferences("SharedPreferencesFile", Activity.MODE_PRIVATE)
+    }
+
+    override fun onSharedPreferenceChanged(sharedPreferences: SharedPreferences?, key: String?) {
+    }
+
+    override fun onStart() {
+        super.onStart()
+        sharedPreferences?.registerOnSharedPreferenceChangeListener(this)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        sharedPreferences?.unregisterOnSharedPreferenceChangeListener(this)
+    }
+}
+
 class SharedPreferencesV2Activity : BaseActivity() {
 
     private val binding by lazy {
-        ActivityDataFlowBinding.inflate(layoutInflater)
+        ActivitySharedPreferenceV2Binding.inflate(layoutInflater)
     }
 
     private val viewModel by viewModel<SharedPreferencesV2ViewModel>()
@@ -48,9 +79,51 @@ class SharedPreferencesV2Activity : BaseActivity() {
         val title = intent.getStringExtra("title")
         binding.tvTitle.text = title
 
+        if (savedInstanceState == null) {
+            supportFragmentManager.commit {
+                replace(binding.frameLayoutTop.id, FrameLayoutTopFragment())
+                replace(binding.frameLayoutBottom.id, FrameLayoutBottomFragment())
+            }
+        }
+
         binding.btnSendData.setOnClickListener {
             viewModel.sendData()
         }
+    }
+}
+
+class FrameLayoutTopFragment : BaseFragment() {
+
+    private lateinit var binding: FragmentSharedPreferenceV2Binding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentSharedPreferenceV2Binding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onSharedPreferenceChanged(sharedPreferences: SharedPreferences?, key: String?) {
+        if (key == "data") {
+            val data = sharedPreferences?.getString("data", "")
+            binding.tvData.text = data
+        }
+    }
+}
+
+class FrameLayoutBottomFragment : BaseFragment() {
+
+    private lateinit var binding: FragmentSharedPreferenceV2Binding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentSharedPreferenceV2Binding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onSharedPreferenceChanged(sharedPreferences: SharedPreferences?, key: String?) {

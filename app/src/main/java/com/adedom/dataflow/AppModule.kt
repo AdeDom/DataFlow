f98a2a44e8a5f2f8993d819cced055de3b2bd647
pathiphon <pathiphon.ja@chococrm.com>
2022-05-30 12:04:58 +0700

    Implement data store on dagger.

--- app/src/main/java/com/adedom/dataflow/AppModule.kt
+++ app/src/main/java/com/adedom/dataflow/AppModule.kt
@@ -1,10 +1,32 @@
 package com.adedom.dataflow
 
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.PreferenceDataStoreFactory
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStoreFile
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
+
+    @Singleton
+    @Provides
+    fun providePreferencesDataStore(@ApplicationContext context: Context): DataStore<Preferences> {
+        return PreferenceDataStoreFactory.create {
+            context.preferencesDataStoreFile("file")
+        }
+    }
+
+    @Singleton
+    @Provides
+    fun provideAppDataStore(dataStoreFile: DataStore<Preferences>): AppDataStore {
+        return AppDataStoreImpl(dataStoreFile)
+    }
 }
--- app/src/main/java/com/adedom/dataflow/SharedPreferencesActivity.kt
+++ app/src/main/java/com/adedom/dataflow/SharedPreferencesActivity.kt
@@ -1,26 +1,27 @@
 package com.adedom.dataflow
 
-import android.content.Context
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.core.longPreferencesKey
-import androidx.datastore.preferences.preferencesDataStore
 import androidx.lifecycle.lifecycleScope
 import com.adedom.dataflow.databinding.ActivityDataFlowBinding
-import kotlinx.coroutines.flow.launchIn
-import kotlinx.coroutines.flow.onEach
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-private val Context.dataStoreFile by preferencesDataStore("file")
-
+@AndroidEntryPoint
 class SharedPreferencesActivity : AppCompatActivity() {
+
+    @Inject
+    lateinit var appDataStore: AppDataStore
 
     private val binding by lazy {
         ActivityDataFlowBinding.inflate(layoutInflater)
     }
-
-    private val currentTime = longPreferencesKey("currentTime")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,16 +29,43 @@ class SharedPreferencesActivity : AppCompatActivity() {
         val title = intent.getStringExtra("title")
         binding.tvTitle.text = title
 
-        dataStoreFile.data.onEach { preferences ->
-            binding.tvData.text = preferences[currentTime].toString()
+        appDataStore.getCurrentTimeFlow().onEach { currentTime ->
+            binding.tvData.text = currentTime.toString()
         }.launchIn(lifecycleScope)
 
         binding.btnSendData.setOnClickListener {
             lifecycleScope.launch {
-                dataStoreFile.edit { preferences ->
-                    preferences[currentTime] = System.currentTimeMillis()
-                }
+                appDataStore.setCurrentTime(System.currentTimeMillis())
             }
+        }
+    }
+}
+
+interface AppDataStore {
+    suspend fun setCurrentTime(currentTime: Long)
+    suspend fun getCurrentTime(): Long?
+    fun getCurrentTimeFlow(): Flow<Long?>
+}
+
+class AppDataStoreImpl(
+    private val dataStoreFile: DataStore<Preferences>,
+) : AppDataStore {
+
+    private val currentTime = longPreferencesKey("currentTime")
+
+    override suspend fun setCurrentTime(currentTime: Long) {
+        dataStoreFile.edit { preferences ->
+            preferences[this.currentTime] = currentTime
+        }
+    }
+
+    override suspend fun getCurrentTime(): Long? {
+        return dataStoreFile.data.first()[currentTime]
+    }
+
+    override fun getCurrentTimeFlow(): Flow<Long?> {
+        return dataStoreFile.data.map { preferences ->
+            preferences[currentTime]
         }
     }
 }

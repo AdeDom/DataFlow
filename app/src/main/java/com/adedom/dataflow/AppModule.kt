865ef575a5bc69beee67b489821432a9cbd9e3df
pathiphon <pathiphon.ja@chococrm.com>
2022-05-30 11:16:34 +0700

    Clean shared pref.

--- app/src/main/java/com/adedom/dataflow/AppModule.kt
+++ app/src/main/java/com/adedom/dataflow/AppModule.kt
@@ -1,31 +1,10 @@
 package com.adedom.dataflow
 
-import android.app.Activity
-import android.content.Context
-import android.content.SharedPreferences
 import dagger.Module
-import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
-import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
-
-    @Singleton
-    @Provides
-    fun provideSharedPreferences(@ApplicationContext context: Context): SharedPreferences =
-        context.getSharedPreferences("SharedPreferencesFile", Activity.MODE_PRIVATE)
-
-    @Singleton
-    @Provides
-    fun provideSharedPref(sharedPreferences: SharedPreferences): SharedPref =
-        SharedPrefImpl(sharedPreferences)
-
-    @Singleton
-    @Provides
-    fun provideDefaultRepository(sharedPref: SharedPref): DefaultRepository =
-        DefaultRepositoryImpl(sharedPref)
 }
--- app/src/main/java/com/adedom/dataflow/SharedPreferencesActivity.kt
+++ app/src/main/java/com/adedom/dataflow/SharedPreferencesActivity.kt
@@ -1,56 +1,19 @@
 package com.adedom.dataflow
 
-import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.edit
 import com.adedom.dataflow.databinding.ActivityDataFlowBinding
-import dagger.hilt.android.AndroidEntryPoint
-import javax.inject.Inject
 
-@AndroidEntryPoint
-class SharedPreferencesActivity : AppCompatActivity(),
-    SharedPreferences.OnSharedPreferenceChangeListener {
+class SharedPreferencesActivity : AppCompatActivity() {
 
     private val binding by lazy {
         ActivityDataFlowBinding.inflate(layoutInflater)
     }
-
-    @Inject
-    lateinit var sharedPreferences: SharedPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
         val title = intent.getStringExtra("title")
         binding.tvTitle.text = title
-
-        binding.btnSendData.setOnClickListener {
-            val data = System.currentTimeMillis().toString()
-            sendData(data)
-        }
-    }
-
-    private fun sendData(data: String) {
-        sharedPreferences.edit {
-            putString("data", data)
-        }
-    }
-
-    override fun onSharedPreferenceChanged(sharedPreferences: SharedPreferences?, key: String?) {
-        if (key == "data") {
-            val data = sharedPreferences?.getString("data", "")
-            binding.tvData.text = data
-        }
-    }
-
-    override fun onStart() {
-        super.onStart()
-        sharedPreferences.registerOnSharedPreferenceChangeListener(this)
-    }
-
-    override fun onStop() {
-        super.onStop()
-        sharedPreferences.unregisterOnSharedPreferenceChangeListener(this)
     }
 }
